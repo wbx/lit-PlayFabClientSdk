cca972268c9bcf4ea6f92c07b28e81954477acd2
Lyrthras <defelixanne@gmail.com>
2023-01-11 22:19:28 +0800

    Change structure for correct lit install paths

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
-    name = "wbx/playfab-client-sdk",
-    version = "0.125.220509-0",
+    name = "wbx/PlayFab",
+    version = "0.125.220509-2",
     description = "PlayFab Client SDK modified for luvit/lit use.",
     license = "Apache License 2.0",
     dependencies = {
